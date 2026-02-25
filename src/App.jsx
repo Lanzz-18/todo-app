59f18e43da8e1288855717fb90b644ea94a4e2ed
Senal Perera <senalperera777@gmail.com>
2026-02-25 23:38:47 +0530

    Added functionality
    - deleting tasks
    - structuring tasks
    - getting data from localStorage and storing to it as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos")
+    return saved ? JSON.parse(saved) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },  [todos]) // whenever todos list change, save it to localstorage
+  
   const [input, setInput] = useState("")
 
   const addTodo =()=>{
     if(input=="") return // refuse added empty tasks
     setTodos([...todos, { id: Date.now(), text: input, completed: false}])
     setInput("") // clear input
+  }
+
+  const deleteTodo = (id) =>{
+    // Filtering out the task to be deleted and setting it out
+    setTodos(todos.filter(todo => todo.id == id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(todos.map(todo => {
+      todo.id === id ? {...todo, completed: !todo.completed } : todo // if id doesn't match, just return the original task
+    }))
   }
 
   return (
@@ -25,7 +44,12 @@ function App() {
       <div className="todo-list">
         {todos.map(todo => (
           <div key={todo.id} className="todo-item">
-            <p>{todo.text}</p>
+            <p 
+              style={{textDecoration: todo.completed ? "line-through" : "none"}}
+              onClick={() => toggleComplete(todo.id)}>
+              {todo.text}
+            </p>
+            <button onClick={() => deleteTodo(todo.id)}>🗑️</button>
           </div>
         ))}
       </div>
